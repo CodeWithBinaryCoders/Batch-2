e26f875506e55774e3b1fd702908aa3586402003
Avik Agarwala <avikagarwala@outlook.com>
2023-12-02 15:45:42 +0530

    output in C

---
+++ output/output.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Binary");
+    printf(" ");
+    printf("Coders\n\n");
+
+    printf("Binary ");
+    printf("Coders");
+
+    printf("\n\nBinary");
+    printf(" Coders\n");
+
+    printf("\nBinary\nCoders\n\n");
+
+    printf("Binary");
+    printf("\n");
+    printf("Coders\n\n");
+
+    printf("Binary\n");
+    printf("Coders\n\n");
+
+    printf("Binary");
+    printf("\nCoders\n");
+
+    return 0;
+}
+
+/*
+OUTPUT
+------
+
+Binary Coders
+
+Binary Coders
+
+Binary Coders
+
+Binary
+Coders
+
+Binary
+Coders
+
+Binary
+Coders
+
+Binary
+Coders
+
+
+*/
