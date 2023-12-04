5f94f1bba5e7eeafc3b9e129da8f54ee573ee8a4
Avik Agarwala <avikagarwala@outlook.com>
2023-12-04 19:05:33 +0530

    output in python

---
+++ output/output.py
@@ -0,0 +1,64 @@
+print('Binary Coders')
+
+print("\nBinary Coders")
+
+print('''Binary Coders''')
+
+print() # new line
+
+print("Binary")
+print("Coders")
+
+print("\nBinary\nCoders\n")
+
+print('''This
+      is
+      Binary
+      Coders\n''')
+
+print('''This
+    is
+Binary
+      Coders\n''')
+
+print("Binary", end = ' ')
+print("Coders\n")
+
+print("Binary", end = '')
+print("Coders\n")
+
+print("This ", end = "is ")
+print("Binary Coders")
+
+'''
+OUTPUT
+------
+
+Binary Coders
+
+Binary Coders
+Binary Coders
+
+Binary
+Coders
+
+Binary
+Coders
+
+This
+      is
+      Binary
+      Coders
+
+This
+    is
+Binary
+      Coders
+
+Binary Coders
+
+BinaryCoders
+
+This is Binary Coders
+
+'''
