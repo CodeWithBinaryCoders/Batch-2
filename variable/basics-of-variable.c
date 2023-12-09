091f29c7787318cf27811b44330d96de99a30156
Alik Agarwala <alikagarwala@outlook.com>
2023-12-09 22:53:54 +0530

    add basics of variable

---
+++ variable/basics-of-variable.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int x; // declaring a variable
+    x = 7; // assigning value 5 to variable x
+
+    printf("The stored value is %d\n", x);
+
+    // int a;
+    // int b;
+    // a = 6;
+    // b = 10;
+
+    // int a, b;
+    // a = 6;
+    // b = 10;
+
+    int a = 6, b = 10;
+
+    printf("Value of a is %d & value of b is %d\n", a, b);
+
+
+    return 0;
+}
+
