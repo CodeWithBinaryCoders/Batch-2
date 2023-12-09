b36597ed8b7bc34efb247d4dce7ea8caaa47f6ee
Alik Agarwala <alikagarwala@outlook.com>
2023-12-10 00:05:42 +0530

    add same for C++

---
+++ variable/data-types-and-variables.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 5;
+    cout << "The value of x is " << x << endl;
+
+    float y = 4.25;
+
+    cout << "The value of y is " << y << endl;
+
+    char ch = 'S';
+
+    cout << "value of ch is " << ch << endl;
+
+    string str = "Alik";
+
+    cout << "value of str is " << str << endl;
+
+    bool check = true;
+
+    cout << "value of check is " << check << endl;
+    return 0;
+}
