393a941698be73735337f961c183fa52d3865877
Alik Agarwala <alikagarwala@outlook.com>
2023-12-09 23:37:44 +0530

    add more about data types

---
+++ variable/more-data-types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    float x = 4.26; // double -> float (implicit type casting)
+    printf("Value of x is %f\n", x);
+
+    float y = 9.23f; // f indicates a float value
+    printf("Value of y is %f\n", y);
+
+    char ch = 'c';
+    printf("The value is %c\n", ch);
+
+
+    return 0;
+}
