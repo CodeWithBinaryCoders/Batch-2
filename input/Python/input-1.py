2b4fcd19e51ddb0c761159db87ff5e5f4ce83e1f
Avik Agarwala <avikagarwala@outlook.com>
2023-12-14 22:51:54 +0530

    input in python

---
+++ input/Python/input-1.py
@@ -0,0 +1,4 @@
+x = input() # asks the user to input something
+y = input()
+
+print(x + y)
---
+++ input/Python/input-2.py
@@ -0,0 +1,5 @@
+# print("Enter value for x : ", end='')
+# x = float(input())
+x = float(input("Enter value for x : "))
+
+print(f"Value of x is {x}")
