744ec482af5acdfc0caf254b97cdb0d9787bd7dd
Alik Agarwala <alikagarwala@outlook.com>
2023-12-24 23:33:54 +0530

    ad boolCpp

---
+++ booleans/boolCPP.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool check = 0;
+    cout << check << endl;
+
+    bool myVar = true;
+    cout << myVar << endl;
+
+    return 0;
+}
+
