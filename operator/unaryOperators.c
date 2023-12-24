aa228376fba35a3816f6b3c725c7453f787586fc
Alik Agarwala <alikagarwala@outlook.com>
2023-12-25 00:01:46 +0530

    add unaryOperator.c

---
+++ operator/unaryOperators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int x = 5;
+
+    x++; // increment by 1
+
+    printf("%d\n", x); // current value is 6
+
+    ++x; // increment by 1
+
+    printf("%d\n", x); // current value is 7
+
+    return 0;
+}
