a4cdb79f3fd5138610fd828268c055b47cbc415f
Alik Agarwala <alikagarwala@outlook.com>
2023-12-24 23:41:15 +0530

    add operator.c

---
+++ operator/operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int x = 6, y = 4;
+
+    printf("Sum is %d\n", x + y);
+    printf("Sub is %d\n", x - y);
+    printf("Product is %d\n", x * y);
+    printf("Quotient is %d\n", x / y);
+    printf("Remainder is %d\n", x % y);
+
+    return 0;
+}
+
