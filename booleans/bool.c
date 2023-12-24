91387780e498164fcf9aae3173f351914c084602
Alik Agarwala <alikagarwala@outlook.com>
2023-12-24 23:06:45 +0530

    add stdbool.h

---
+++ booleans/bool.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    _Bool check = true; // _Bool check = 1;
+    printf("%d\n", check);
+
+    bool myVar = false; // _Bool myVar = 0;
+    printf("%d\n", myVar);
+
+    return 0;
+}
+
