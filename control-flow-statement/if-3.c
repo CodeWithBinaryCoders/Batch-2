1af2655c1c7afb5ef1fc262a85aa70c84c0941c7
Alik Agarwala <alikagarwala@outlook.com>
2024-01-05 00:18:10 +0530

    if-3.c

---
+++ control-flow-statement/if-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int x = 5;
+    int y = 5;
+    int z = 7;
+
+    if (x == y && z == 6)
+        printf("Yayyy!!!\n");
+
+    printf("Mehh!\n");
+
+    return 0;
+}
