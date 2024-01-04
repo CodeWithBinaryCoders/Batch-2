8b0264e30789b023dc6d9d9edf98f5620c5de984
Alik Agarwala <alikagarwala@outlook.com>
2024-01-05 00:32:32 +0530

    ad else-if-1.c

---
+++ control-flow-statement/else-if-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int amount = 56;
+
+    if (amount >= 100)
+        printf("Go to Central\n");
+    else if (amount >= 50)
+        printf("Go to Rabindra Sarobar\n");
+    else
+        printf("Go to Maidan\n");
+
+    return 0;
+}
+
+
+
