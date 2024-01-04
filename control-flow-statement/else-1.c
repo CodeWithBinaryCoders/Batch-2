46af82ab7a6f33a4eb8c07d2aa078b26b55316bb
Alik Agarwala <alikagarwala@outlook.com>
2024-01-05 00:24:29 +0530

    add else-1.c

--- control-flow-statement/else-1.c
+++ control-flow-statement/else-1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    
+    int amount = 100;
+
+    if (amount >= 1000)
+        printf("Go for movie\n");
+    else
+        printf("Tea date\n");
+
     return 0;
 }
