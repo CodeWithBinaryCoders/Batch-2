010329a2b0b6cb84083ee95d47cd6395fbcac16f
Alik Agarwala <alikagarwala@outlook.com>
2024-01-04 23:41:34 +0530

    logical operator extra

--- operator/logicalOperatorExtra.cpp
+++ operator/logicalOperatorExtra.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
-    
+    cout << (5 || 6) << endl;
+
+    cout << (5 && 7) << endl;
+
+    cout << (0 && 6) << endl;
+
+    cout << (6 && 0) << endl;
+
+    cout << (6 && 6) << endl;
+
+    cout << !(6 && 6) << endl;
+
     return 0;
 }
