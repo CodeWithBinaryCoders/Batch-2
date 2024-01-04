de4c34d2ab0a7b1a57545eac8c68b671ff67437a
Alik Agarwala <alikagarwala@outlook.com>
2024-01-05 00:07:40 +0530

    add if.c

---
+++ control-flow-statement/if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int x = 5;
+    int y = 6;
+
+    if (x < y)
+    {
+        printf("x is less than y\n");
+        printf("Hurray!!!\n");
+    }
+
+    printf("Rock you...\n");
+
+    return 0;
+}
