231f185a7f0c77171a5e1b47cd4c43bc41d09ef4
Alik Agarwala <alikagarwala@outlook.com>
2024-01-13 23:35:39 +0530

    ex-2.cpp

---
+++ control-flow-statement/switch-case/exercise-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    switch (n)
+    {
+    case 1:
+        cout << "1" << endl;
+
+    case 2:
+        cout << "2" << endl;
+
+    default:
+        cout << "default" << endl;
+
+    case 3:
+        cout << "3" << endl;
+
+    case 4:
+        cout << "4" << endl;
+    }
+
+    return 0;
+}
