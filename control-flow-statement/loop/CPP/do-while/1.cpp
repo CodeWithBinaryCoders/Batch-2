137104f5fa541a48bd06570d8309698bbe59539e
Avik Agarwala <avikagarwala@outlook.com>
2024-02-02 23:42:22 +0530

    do while loop in python

---
+++ control-flow-statement/loop/CPP/do-while/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0; // initialization
+
+    do
+    {
+        cout << "Value of i is " << i << endl;
+    } while (i != 0);
+
+    return 0;
+    
+}
+
