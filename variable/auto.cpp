7f0bf303b808861fd1f260dd9023893c743f94d3
Alik Agarwala <alikagarwala@outlook.com>
2024-02-06 23:21:48 +0530

    auto keyword

---
+++ variable/auto.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    auto str = "BCD";
+    auto x = 5;
+    auto y = 4.67;
+
+    cout << str << " " << x << " " << y << endl;
+
+    return 0;
+}
+
