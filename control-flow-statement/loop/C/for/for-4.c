d1202fdff8c9434b9c47fd6d7e0c659632a5d25d
Alik Agarwala <alikagarwala@outlook.com>
2024-02-07 00:13:37 +0530

    for-4.c

---
+++ control-flow-statement/loop/C/for/for-4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+
+    for (i = 1; i <= 5; i++)
+    {
+        printf("Hello\n");
+    }
+    
+    printf("Value of i is %d\n", i);
+
+    return 0;
+}
+
+
+
