66a95a59cc40f28c41303d7b731782b66bb7643d
Alik Agarwala <alikagarwala@outlook.com>
2024-02-21 22:48:25 +0530

    continue-1.cpp

---
+++ control-flow-statement/jump-statements/continue-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        if (i == 3)
+            continue;
+        
+        cout << i << endl;
+    }
+
+    return 0;
+}
+
