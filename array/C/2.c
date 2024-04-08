45215e47bdcb915a156249e645bc338f7b26d6d2
Avik Agarwala <avikagarwala@outlook.com>
2024-04-08 23:04:02 +0530

    2.c

---
+++ array/C/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char luckyAlphabet[5] = {'A', 'B', 'C', 'D', 'E'}; // assigning data at the time of declaration
+
+    printf("%c\n", luckyAlphabet[0]);
+    printf("%c\n", luckyAlphabet[1]);
+    printf("%c\n", luckyAlphabet[2]);
+    printf("%c\n", luckyAlphabet[3]);
+    printf("%c\n", luckyAlphabet[4]);
+
+    return 0;
+}
+
+
