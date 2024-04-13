fd6265da6f80e3895f278cf4c298605831a439e3
Avik Agarwala <avikagarwala@outlook.com>
2024-04-13 22:56:04 +0530

    update code

--- array/CPP/4.cpp
+++ array/CPP/4.cpp
@@ -14,7 +14,7 @@ int main()
     cout << sizeof (5.8f) << ", "; // 4 as float
     cout << sizeof (char) << ", "; // 1
     cout << sizeof ('A') << ", "; // 1
-    cout << sizeof ((int)5.9) << endl << endl; // 1 as typecasted to int
+    cout << sizeof ((int)5.9) << endl << endl; // 4 as typecasted to int
 
     cout << sizeof a << ", "; // 4
     cout << sizeof a + 3 << ", "; // 7 as 4 + 3
@@ -23,7 +23,7 @@ int main()
     cout << sizeof 5.8f << ", "; // 4 as float
     cout << sizeof (char) << ", "; // 1
     cout << sizeof 'A' << ", "; // 1
-    cout << sizeof ((int)5.9) << endl << endl; // 1 as typecasted to int
+    cout << sizeof ((int)5.9) << endl; // 4 as typecasted to int
     return 0;
 }
 
