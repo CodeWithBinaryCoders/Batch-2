7e954b664d750516e4697f05a56c03e24b5ed23e
Avik Agarwala <avikagarwala@outlook.com>
2024-04-13 22:39:21 +0530

    array length in cpp

---
+++ array/CPP/3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {45, 345, 234, 12, 76, -6, 0, 84, -68, 1};
+
+    cout << "Length of the array is " << sizeof (arr) / sizeof (int) << endl;
+
+    return 0;
+}
