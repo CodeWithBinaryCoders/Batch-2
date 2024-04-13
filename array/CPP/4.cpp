5749b21a2ca8123cb5584e646ed28a62398be069
Avik Agarwala <avikagarwala@outlook.com>
2024-04-13 22:51:57 +0530

    update code

--- array/CPP/4.cpp
+++ array/CPP/4.cpp
@@ -7,12 +7,23 @@ using namespace std;
 int main()
 {
     int a;
-    cout << sizeof (a) << endl; // 4
-    cout << sizeof (45) << endl; // 4
-    cout << sizeof (5.8) << endl; // 8 as double
-    cout << sizeof (5.8f) << endl; // 4 as float
-    cout << sizeof (char) << endl; // 1
-    cout << sizeof ('A') << endl; // 1
-    cout << sizeof ((int)5.9) << endl; // 1 as typecasted to int
+    cout << sizeof (a) << ", "; // 4
+    cout << sizeof (a + 3) << ", "; // 4
+    cout << sizeof (45) << ", "; // 4
+    cout << sizeof (5.8) << ", "; // 8 as double
+    cout << sizeof (5.8f) << ", "; // 4 as float
+    cout << sizeof (char) << ", "; // 1
+    cout << sizeof ('A') << ", "; // 1
+    cout << sizeof ((int)5.9) << endl << endl; // 1 as typecasted to int
+
+    cout << sizeof a << ", "; // 4
+    cout << sizeof a + 3 << ", "; // 7 as 4 + 3
+    cout << sizeof 45 << ", "; // 4
+    cout << sizeof 5.8 << ", "; // 8 as double
+    cout << sizeof 5.8f << ", "; // 4 as float
+    cout << sizeof (char) << ", "; // 1
+    cout << sizeof 'A' << ", "; // 1
+    cout << sizeof ((int)5.9) << endl << endl; // 1 as typecasted to int
     return 0;
 }
+
