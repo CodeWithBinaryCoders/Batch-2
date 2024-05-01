e17bc14ab7872649ea2ede105be4e3aedc038975
Alik Agarwala <alikagarwala@outlook.com>
2024-05-02 00:10:48 +0530

    matrix input-output in c

---
+++ 2D-array/matrix-input-output.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    // take row and col value as input
+    int row, col;
+    scanf("%d%d", &row, &col);
+
+    // declare the 2D array
+    int arr[row][col];
+
+    // take inputs
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < col; j++)
+            scanf("%d", &arr[i][j]);
+
+    // output
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+            printf("%d ", arr[i][j]);
+        printf("\n");
+    }
+
+    printf("\n");
+
+    return 0;
+}
