304f1a3a8197c89a2d8ac6031b62d4fc2ef1a52a
Alik Agarwala <alikagarwala@outlook.com>
2024-05-02 00:16:40 +0530

    minor change

--- 2D-array/matrix-input-output.c
+++ 2D-array/matrix-input-output.c
@@ -17,12 +17,13 @@ int main()
             scanf("%d", &arr[i][j]);
 
     printf("\n");
-    
+
     // output
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
             printf("%d ", arr[i][j]);
+        
         printf("\n");
     }
 
