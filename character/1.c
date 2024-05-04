2b109d7de06022d30201f3a21f94119670e0435b
Alik Agarwala <alikagarwala@outlook.com>
2024-05-04 23:24:38 +0530

    ASCII in c

---
+++ character/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    ch = 'A';
+    printf("%c\n", ch);
+
+    return 0;
+}
+
---
+++ character/working-with-ASCII.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char ch = 'A'; // 65
+    printf("%d\n", ch);
+
+    int x = 98; // b
+    printf("%c\n", x);
+
+    ++x; // 99
+    printf("%c\n", x);
+
+    ch++;
+    printf("%c\n", ch);
+
+    return 0;
+}
