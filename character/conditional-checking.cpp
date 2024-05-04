a634b6b1670aa6ea65dd41ea638be4e857adf288
Alik Agarwala <alikagarwala@outlook.com>
2024-05-04 23:30:07 +0530

    conditional checking with char

---
+++ character/conditional-checking.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+
+    cin >> ch;
+
+    if (ch > 'p')
+        cout << "Success" << endl;
+    else
+        cout << "Fail" << endl;
+
+    return 0;
+}
+
