474dcaf5cc15e81a8c584ac1e6b1953ca04b7b8f
Alik Agarwala <alikagarwala@outlook.com>
2024-05-24 00:03:52 +0530

    3.cpp

---
+++ string/CPP/3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str;
+
+    getline(cin, str); // This will take the whole line as input
+
+    cout << str << endl;
+
+    return 0;
+}
