22be2a8e2c2b1abd89db403b8494633d5824d7bb
Alik Agarwala <alikagarwala@outlook.com>
2024-05-24 00:10:30 +0530

    6.cpp

---
+++ string/CPP/6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        cout << str[i];;
+    }
+
+    cout << endl;
+
+    return 0;
+}
