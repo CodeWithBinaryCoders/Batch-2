477cfe5c65d0c46c4d4420862e46fa07a0fe2b60
Alik Agarwala <alikagarwala@outlook.com>
2024-05-24 00:06:35 +0530

    4.cpp

---
+++ string/CPP/4.cpp
@@ -0,0 +1,15 @@
+// access character in string by index
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str;
+    str = "Hello World";
+
+    cout << str[0] << endl; // H
+    cout << str[1] << endl; // e
+    cout << str[2] << endl; // l
+
+    return 0;
+}
