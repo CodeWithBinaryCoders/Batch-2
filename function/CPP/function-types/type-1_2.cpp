51d371e7e2aedfabd5a64c2d01f3260d3f91564c
Avik Agarwala <avikagarwala@outlook.com>
2024-06-20 23:59:51 +0530

    type-1_2

---
+++ function/CPP/function-types/type-1_2.cpp
@@ -0,0 +1,22 @@
+// no return, no parameter
+
+#include <iostream>
+using namespace std;
+
+void func(); // function prototype
+
+int main()
+{
+    func(); // call the function, no parameter is provided
+
+    cout << "Program end" << endl;
+
+    return 0;
+}
+
+void func() // function definition
+{
+    cout << "Hello, World!" << endl;
+    return;
+    cout << "Binary Learn" << endl;
+}
