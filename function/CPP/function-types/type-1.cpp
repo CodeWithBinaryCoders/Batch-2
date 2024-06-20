9be49656cfe9add523964349d4ed220ca466394f
Avik Agarwala <avikagarwala@outlook.com>
2024-06-20 23:50:47 +0530

    add function type 1

---
+++ function/CPP/function-types/type-1.cpp
@@ -0,0 +1,24 @@
+// no return, no parameter
+
+#include <iostream>
+using namespace std;
+
+void sum(); // function prototype
+
+int main()
+{
+    sum(); // call the function, no parameter is provided
+
+    cout << "Program end" << endl;
+
+    return 0;
+}
+
+void sum() // function definition
+{
+    int a, b; // local varible
+    cout << "Enter two integers : ";
+    cin >> a >> b; // input
+    cout << "Sum is " << a + b << endl;
+}
+
