3d630df48f566c16e8a22b1cb4000d910fa7aae2
Alik Agarwala <alikagarwala@outlook.com>
2024-08-11 23:59:38 +0530

    sum of n natural numbers

---
+++ recursion/C/sum-of-n-natural-numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum(int n)
+{
+    if (n == 1)
+        return 1;
+    
+    return n + sum(n - 1);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    printf("%d\n", sum(n));
+
+    return 0;
+}
