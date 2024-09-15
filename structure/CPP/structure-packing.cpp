a2c42bb71575d68026281af0e710f57ba41161f5
Alik Agarwala <alikagarwala@outlook.com>
2024-09-16 00:16:08 +0530

    packing

---
+++ structure/CPP/structure-packing.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#pragma pack(1) // to enable structure packing
+using namespace std;
+
+struct Test
+{
+    char a; // 1 byte
+    int b; // 4 bytes
+    char c; // 1 byte
+};
+
+struct NewStructure
+{
+    char a; // 1 byte
+    char c; // 1 byte
+    int b; // 4 bytes
+};
+
+int main()
+{
+    cout << "Hypothetical size of Test: " << sizeof(char) + sizeof(int) + sizeof(char) << endl;
+    cout << "Size of Test: " << sizeof(Test) << endl;
+
+    cout << "Hypothetical size of NewStructure: " << sizeof(char) + sizeof(char) + sizeof(int) << endl;
+    cout << "Size of NewStructure: " << sizeof(NewStructure) << endl;
+
+    return 0;
+}
