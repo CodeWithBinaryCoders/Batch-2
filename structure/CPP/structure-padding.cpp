c10f5d346118c5fe07322c3943c300d56d4a62a6
Alik Agarwala <alikagarwala@outlook.com>
2024-09-16 00:08:44 +0530

    padding 2

--- structure/CPP/structure-padding.cpp
+++ structure/CPP/structure-padding.cpp
@@ -8,10 +8,20 @@ struct Test
     char c; // 1 byte
 };
 
+struct NewStructure
+{
+    char a; // 1 byte
+    char c; // 1 byte
+    int b; // 4 bytes
+};
+
 int main()
 {
     cout << "Hypothetical size of Test: " << sizeof(char) + sizeof(int) + sizeof(char) << endl;
     cout << "Size of Test: " << sizeof(Test) << endl;
+
+    cout << "Hypothetical size of NewStructure: " << sizeof(char) + sizeof(char) + sizeof(int) << endl;
+    cout << "Size of NewStructure: " << sizeof(NewStructure) << endl;
 
     return 0;
 }
