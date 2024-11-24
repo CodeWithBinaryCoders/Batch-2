793eea8419c1d1b8dabb60f9214ace4d543881fd
Avik Agarwala <avikagarwala@outlook.com>
2024-11-24 23:50:07 +0530

    bubble sort improved in CPP

--- sorting/bubble-sort/CPP/2.cpp
+++ sorting/bubble-sort/CPP/2.cpp
@@ -30,14 +30,20 @@ void bubbleSort(int *arr, int n) // function definition
 {
     for (int pass = 1; pass <= n - 1; pass++) // handling pass
     {
+        bool flag = false; // to check if any swapping is done in this pass
+
         for (int i = 0; i <= n - pass - 1; i++) // handling traversal
             if (arr[i] > arr[i + 1]) // check if they are in correct order
             {
+                flag = true; // swap happened
+
                 // swap
                 int temp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;
             }
+
+        if (!flag) // if no swapping is done in this pass
+            break; // exit the loop
     }
 }
-
