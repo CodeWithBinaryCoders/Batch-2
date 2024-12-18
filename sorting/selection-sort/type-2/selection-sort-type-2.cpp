e7752a4901a6e7d9bc5cad1b035c112143b91f4f
Alik Agarwala <alikagarwala@outlook.com>
2024-12-18 21:13:49 +0530

    fix: adjust loop condition in selection sort type 2 implementation

--- sorting/selection-sort/type-2/selection-sort-type-2.cpp
+++ sorting/selection-sort/type-2/selection-sort-type-2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void selectionSort(int arr[], int n)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - 1; i++)
         for (int j = i + 1; j < n; j++)
             if (arr[j] < arr[i])
                 swap(arr[j], arr[i]);
